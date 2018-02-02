0a6e0bda77c44a880f7a8623203e2019afcf90b5
zubairabid <zubairabid1999+github@gmail.com>
2018-02-02 15:22:28 +0530

    I'm fucking up Laaaabs

--- Lab/4/bintree.c
+++ Lab/4/bintree.c
@@ -87,8 +87,92 @@ Llint search(Node* head, Llint key) {
 	}
 }
 
-Node * succ(Node* head) {
-	
+Node * searchNode(Node* head, Llint key) { // assumes that the node exists
+	if(head->v == key) {
+		return head;
+	}
+	else if(key < head->v) {
+		if(head->left != NULL)
+			return search(head->left, key);
+		else
+			return NULL;
+	}
+	else if(key > head->v) {
+		if(head->right != NULL)
+			return search(head->right, key);
+		else
+			return NULL;
+	}
+}
+
+Node * succ(Node* head) { // head is the node for which we seek successor
+  if(head->right != NULL) { // if there is an immediate right
+
+	}
+	else if (head->parent != NULL){ // look back
+
+	}
+	else if (head->parent == NULL) { // no successor then
+
+	}
+}
+
+Node * delete(Node* head, Llint val) { // the head has to be the overall tree head !!!! RECONSIDER. Make it the parent
+
+	if (search(head, val) == 0) {
+		printf("The value %lld does not exist.\n", val);
+		return head;
+	}
+
+	Node * point = searchNode(head, val);
+	Node * temp;
+
+	// if the node we want to delete is a leaf, delete the Node
+	if(point->right == NULL && point->left == NULL) {
+		temp = point->parent;
+		if(temp->right->v == val) {
+			temp->right = NULL;
+
+		}
+		else if (temp->left->v == val) {
+			temp->left = NULL;
+		}
+		free(point);
+	}
+
+	// if the node we want to delete has a single child, remove node and attach the child to parent
+	else if (point->right == NULL && point->left != NULL) { // child is to the left
+		temp = point->parent; // parent of the point
+		if (temp->right->v == val) { // if the point is the right child of parent
+			temp->right = point->left;
+		}
+		else if(temp->left->v == val) { // if the point is the left child of parent
+				temp->left = point->left;
+		}
+
+		point->left->parent = temp;
+		free(point);
+	}
+	else if (point->left == NULL && point->right != NULL) { // child is to the right
+		temp = point->parent; // parent of the point
+		if (temp->right->v == val) { // if the point is the right child of parent
+			temp->right = point->right;
+		}
+		else if(temp->left->v == val) { // if the point is the left child of parent
+				temp->left = point->right;
+		}
+
+		point->right->parent = temp;
+		free(point);
+	}
+
+	// otherwise, replace the node value with successor, and then delete that in the subtree
+	else {
+		Node* successor = succ(point);
+		point->v = successor->v;
+		return delete(point->right, successor->v);
+	}
+
 }
 
 Llint dfs(Node* head, Llint key) {
