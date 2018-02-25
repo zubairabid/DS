3f552baabb32c215c2d61957b9efa6bd893ea57c
zubairabid <zubairabid1999+github@gmail.com>
2018-02-25 12:02:00 +0530

    C_1 accepted

--- Ass/2/C_1.cpp
+++ Ass/2/C_1.cpp
@@ -12,12 +12,12 @@ typedef struct data {
   lint sadness;
 } girl;
 
-girl * hip[200010]; // stores the heap
+girl hip[200010]; // stores the heap
 lint ind = 0; // stores the last element of the heap
 
 void print() {
   for(lint i = 1; i <= ind; i++) {
-    std::cout << hip[i]->sadness << ' ';
+    std::cout << hip[i].sadness << ' ';
   }
   std::cout  << '\n';
 }
@@ -30,10 +30,10 @@ void heapify(lint index) { // heapifies the heap index up, by parent
     return;
   }
 
-  girl * temp;
+  girl temp;
 
   // std::cout << "Checking validity at heap[" << index << "] = " << hip[index] << ", parent is heap[" << index/2 << "] as " << hip[index/2] << std::endl;
-  if(hip[index]->sadness > hip[index/2]->sadness) { // Max heap. Parent must be >= child
+  if(hip[index].sadness > hip[index/2].sadness) { // Max heap. Parent must be >= child
     // swap
     temp = hip[index/2];
     hip[index/2] = hip[index];
@@ -46,7 +46,7 @@ void heapify(lint index) { // heapifies the heap index up, by parent
 }
 
 void heapdown(lint index) {
-  girl * temp;
+  girl temp;
 
   if(2*index > ind) {
     return;
@@ -61,7 +61,7 @@ void heapdown(lint index) {
 
   if(2*index+1 > ind)
   {
-    if(hip[index]->sadness < hip[2*index]->sadness){
+    if(hip[index].sadness < hip[2*index].sadness){
       temp = hip[index];
       hip[index] = hip[2*index];
       hip[2*index] = temp;
@@ -70,8 +70,8 @@ void heapdown(lint index) {
   }
 
 
-  if(hip[index]->sadness < hip[2*index]->sadness || hip[index]->sadness < hip[2*index+1]->sadness) {
-    if(hip[2*index]->sadness > hip[2*index+1]->sadness) {
+  if(hip[index].sadness < hip[2*index].sadness || hip[index].sadness < hip[2*index+1].sadness) {
+    if(hip[2*index].sadness > hip[2*index+1].sadness) {
       temp = hip[index];
       hip[index] = hip[2*index];
       hip[2*index] = temp;
@@ -87,7 +87,7 @@ void heapdown(lint index) {
   return;
 }
 
-void hinsert(girl * data) { // Assumes the rest of the heap is a heap proper
+void hinsert(girl data) { // Assumes the rest of the heap is a heap proper
   hip[++ind] = data;
   heapify(ind);
   // std::cout << heapify(ind) << '\n';
@@ -117,7 +117,7 @@ void hdel(lint index) {
   //}
   //free(temp);
 }
-girl *go[1000099];
+girl go[1000099];
 int cmpfunc(const void* a,const void* b)
 {
   girl* x = (girl *)a;
@@ -141,19 +141,19 @@ int main() {
     // reserve 10 for all din[] elements
 
     for(j = 0; j < N; j++) { // for test i
-      girl * g = (girl *) malloc(sizeof(girl));
-      std::cin >> g->arr >> g->days >> g->sadness; // TODO ADD INPUT FOR DAY OF ENTRY
+      girl g;
+      std::cin >> g.arr >> g.days >> g.sadness; // TODO ADD INPUT FOR DAY OF ENTRY
       go[j]=g;
       // std::cout << "Pushed girl with sadness=" << g->sadness << " days=" << g->days << " onto list for day " << temp << '\n';
       //hinsert(g);
       //print();
     }
     lint ptr=0;
-    //qsort(go,N,sizeof(girl),cmpfunc);
+    qsort(go,N,sizeof(girl),cmpfunc);
     // iterate through each day
-    std::cout << "." << '\n';
-    for(long long int k=0;k<N;++k)
-      std::cout<<go[k]->sadness<<std::endl;
+    //std::cout << "." << '\n';
+    // for(long long int k=0;k<N;++k)
+    // std::cout<<go[k].sadness<<std::endl;
     for(j = 1; j <= D; j++) {
       // std::cout << "On day " << (j) << " we have\n";
 
@@ -165,9 +165,10 @@ int main() {
         hinsert(g);
       }*/
 
-      while(ptr<N && (go[ptr]->arr)==j)
+      while(ptr<N && (go[ptr].arr)==j)
       {
-        std::cout << "." << '\n';
+        //std::cout << "." << '\n';
+        sad += go[ptr].sadness * go[ptr].days;
         hinsert(go[ptr]);
         ++ptr;
       }
@@ -183,15 +184,15 @@ int main() {
       // print();
       // std::cout << "Total sadness at start of day: " << sad << '\n';
       // now remove max sadness for the day;
-      sad -= hip[1]->sadness;
-      hip[1]->days = hip[1]->days - 1;
+      sad -= hip[1].sadness;
+      hip[1].days = hip[1].days - 1;
 
       if(sad < 0) {
         sad = 0;
       }
 
       // std::cout << "removed " << hip[1]->sadness << " from sadness. Days left for current : " << hip[1]->days << '\n';
-      if(hip[1]->days == 0) {
+      if(hip[1].days == 0) {
         // std::cout << "We bout to get deleting baby" << '\n';
         hdel(1);
       }
@@ -199,7 +200,7 @@ int main() {
       // std::cout << "Total sadness at end of day: " << sad << '\n';
     }
 
-    std::cout<<sad<<'\n'; // print the minimum sadness for the test
+    std::cout<<sad<<std::endl; // print the minimum sadness for the test
 
   }
 
