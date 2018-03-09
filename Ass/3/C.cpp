174ae7e21fe32001c5d9389bfdb4d52504304917
zubairabid <zubairabid1999+github@gmail.com>
2018-03-10 00:27:59 +0530

    C does not work. Redoing the entire search from scratch.

--- Ass/3/C.cpp
+++ Ass/3/C.cpp
@@ -12,6 +12,10 @@ struct Trie {
   LL length; // stores the length of the word depicted by priority
   bool word; // signifies if the node contains a word end or not
   Trie* edges[MAX];
+};
+
+LL minimize(LL a, LL b) {
+  return a<b?a:b;
 }
 
 // Transforms the character into the integer index equivalent
@@ -43,11 +47,59 @@ void addWord(Trie* root, string word, LL prty, LL len) {
     root->edges[child] = (Trie*)(malloc(sizeof(Trie)));
     init(root->edges[child], prty, len);
   }
-  addWord(edges[child], word.substr(1), prty, len);
+  addWord(root->edges[child], word.substr(1), prty, len);
 }
 
-LL searchWord(Trie* root, string key) {
+/*
+  1st character is always entered manually.
+  We update the count of steps at every priority change, word end, and new form
+*/
+LL searchWord(Trie* root, string key, LL level, LL pc) {
+  char fchar;
+  LL child = 0;
 
+  if(key.length() > 0) {
+    char fchar = key[0];
+    LL child = transform(fchar);
+
+    cout << "Looking for character " << fchar << " in key " << key << ", child is " << child << "\n";
+    cout << (root->edges[child] != NULL) << '\n';
+  }
+  LL count = 0;
+
+  if(level == 0) { // if we're just starting
+    if(root->edges[child] == NULL) {
+      pc = 1;
+      count += 1;
+      cout << "Just started. pc = " << pc << ". count = " << count << ". Calling next\n";
+      count += searchWord(root, key.substr(1), level + 1, pc);
+      return count;
+    }
+    else {
+      cout << "Since it exists not i need not bother\n";
+      return key.length();
+    }
+  }
+  if(key.length() == 0) { // end of the key word
+    cout << "Key ended. Adding to count minimum of " << (level - pc) << " and " << -(1+root->length - level) << ".\n";
+    cout << level << " " << root->length << '\n';
+    count += minimize((level - pc), -(1 + (root->length - level))); // minimum of indi steps vs tab completion
+    return count;
+  }
+  if(root->word == true) { // priority change after
+    cout << "Priority change after this node. Adding to count minimum of " << (level - pc) << " and " << -(1+root->length - level) << ".\n";
+    count += minimize((level - pc), -(1 + (root->length - level))); // minimum of indi steps vs tab completion
+    pc = level;
+    cout << "New pc = " << level << "\n";
+  }
+  if(root->edges[child] != NULL) { // Manually add everything
+    cout << "Rest of key does not exist in trie. It shall be added character by character, so " << key.length() << " characters of " << key << '\n';
+    count += (key.length());
+    return count;
+  }
+  cout << "Calling on the next set\n";
+  count += searchWord(root, key.substr(1), level + 1, pc);
+  return count;
 }
 
 int main() {
@@ -55,6 +107,7 @@ int main() {
   string temp;
 
   Trie* root = (Trie*)(malloc(sizeof(Trie)));
+  init(root, -1, -1); // the root will be -1, -1 priority and length
 
   cin >> M >> N;
   for(i = 0; i < M; i++) {
@@ -62,8 +115,17 @@ int main() {
     addWord(root, temp, (M-i), temp.length());
   }
 
+  for(i = 0; i < MAX; i++) {
+    cout << root->edges[i] << " ";
+  }
+  cout << '\n';
+  for(i = 0; i < MAX; i++) {
+    cout << (root->edges[i]!=NULL) << " ";
+  }
+  cout << '\n';
+
   for(i = 0; i < N; i++) {
     cin >> temp;
-
+    cout << searchWord(root, temp, 0, 0) << '\n';
   }
 }
