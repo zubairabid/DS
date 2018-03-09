b019a6b754f2a32863c2f1d632af30e0698e42db
zubairabid <zubairabid1999+github@gmail.com>
2018-03-10 04:16:58 +0530

    RUN ERROR

--- Ass/3/C.cpp
+++ Ass/3/C.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 typedef long long LL;
 const LL MAX = 26;
+LL timer;
 
 struct Trie {
   LL priority; // stores the max priority of all the possible words
@@ -38,68 +39,68 @@ void init(Trie* node, LL prty, LL len) {
 void addWord(Trie* root, string word, LL prty, LL len) {
   if(word.length() == 0) {
     root->word = true;
+    // cout << "UPDATED WORD\nnode " << root << " : priority = " << root->priority << ". length = " << root->length << ". word = " << root->word << "\n";
     return;
   }
   char fchar = word[0];
   LL child = transform(fchar);
 
+  // cout << "Adding " << fchar << " to " << root << ". edge[" << child << "] = " << root->edges[child] << "\n";
+
   if(root->edges[child] == NULL) { // new branch. Modify priority and length
+    // cout << "New node being created as NULL.\n";
     root->edges[child] = (Trie*)(malloc(sizeof(Trie)));
     init(root->edges[child], prty, len);
+    // cout << "New ";
   }
+  // cout << "node " << root->edges[child] << " : priority = " << root->edges[child]->priority << ". length = " << root->edges[child]->length << ". word = " << root->edges[child]->word << "\n";
   addWord(root->edges[child], word.substr(1), prty, len);
 }
 
-/*
-  1st character is always entered manually.
-  We update the count of steps at every priority change, word end, and new form
-*/
-LL searchWord(Trie* root, string key, LL level, LL pc) {
+void searchWord(Trie * root, string key, LL level, LL pc, bool step1) {
+  LL child;
   char fchar;
-  LL child = 0;
+
+  // cout << "key: " << key << ". level: " << level << ". pc: " << pc << ". node:" << root << "\n";
 
   if(key.length() > 0) {
-    char fchar = key[0];
-    LL child = transform(fchar);
-
-    cout << "Looking for character " << fchar << " in key " << key << ", child is " << child << "\n";
-    cout << (root->edges[child] != NULL) << '\n';
+    fchar = key[0];
+    child = transform(fchar);
   }
-  LL count = 0;
 
-  if(level == 0) { // if we're just starting
+  if(step1) {
     if(root->edges[child] == NULL) {
-      pc = 1;
-      count += 1;
-      cout << "Just started. pc = " << pc << ". count = " << count << ". Calling next\n";
-      count += searchWord(root, key.substr(1), level + 1, pc);
-      return count;
+      timer += key.length();
+      // cout << "Timer after mismatch in step step: " << timer << "\n";
+      return;
     }
-    else {
-      cout << "Since it exists not i need not bother\n";
-      return key.length();
-    }
+    timer += 1;
+    // cout << "Timer after step1 from completion: " << timer << "\n";
+    pc += 1;
+    searchWord(root->edges[child], key.substr(1), level+1, pc, false);
+    return;
   }
-  if(key.length() == 0) { // end of the key word
-    cout << "Key ended. Adding to count minimum of " << (level - pc) << " and " << -(1+root->length - level) << ".\n";
-    cout << level << " " << root->length << '\n';
-    count += minimize((level - pc), -(1 + (root->length - level))); // minimum of indi steps vs tab completion
-    return count;
+
+  if(key.length() == 0) { // in case the element is entirely within the system
+    timer += minimize(level - pc, 1 + root->length - level);
+    // cout << "Timer after key exhausts: " << timer << "\n";
+    return;
   }
-  if(root->word == true) { // priority change after
-    cout << "Priority change after this node. Adding to count minimum of " << (level - pc) << " and " << -(1+root->length - level) << ".\n";
-    count += minimize((level - pc), -(1 + (root->length - level))); // minimum of indi steps vs tab completion
+  // cout << "root->word: " << root->word << "\n";
+  if(/*root->word*/(root->edges[child] == NULL) || ((root->edges[child]->priority) != (root->priority))) { // there will be a priority shift
+    timer += minimize(level - pc, 1 + root->length - level);
+    // cout << "Timer after priority shift: " << timer << "\n";
     pc = level;
-    cout << "New pc = " << level << "\n";
   }
-  if(root->edges[child] != NULL) { // Manually add everything
-    cout << "Rest of key does not exist in trie. It shall be added character by character, so " << key.length() << " characters of " << key << '\n';
-    count += (key.length());
-    return count;
+
+  // cout << "fchar: " << fchar << ". child: " << child << ". edges[child]: " << root->edges[child] << "\n";
+  // cout << "fchar priority: " << root->priority << ". child priority: " << root->edges[child]->priority << "\n";
+  if(root->edges[child] == NULL) { // if the edge does not exist
+    timer += key.length();
+    // cout << "Timer after mismatch: " << timer << "\n";
+    return;
   }
-  cout << "Calling on the next set\n";
-  count += searchWord(root, key.substr(1), level + 1, pc);
-  return count;
+  searchWord(root->edges[child], key.substr(1), level+1, pc, ((root->edges[child]->priority) != (root->priority)));
 }
 
 int main() {
@@ -115,17 +116,49 @@ int main() {
     addWord(root, temp, (M-i), temp.length());
   }
 
-  for(i = 0; i < MAX; i++) {
-    cout << root->edges[i] << " ";
-  }
-  cout << '\n';
-  for(i = 0; i < MAX; i++) {
-    cout << (root->edges[i]!=NULL) << " ";
-  }
-  cout << '\n';
+  // TRIE TESTS. POSITIVE RESULT.
+  // for(i = 0; i < MAX; i++) {
+  //   cout << (root->edges[i]) << '\t';
+  // }
+  // cout << '\n';
+  //
+  // for(i = 0; i < MAX; i++) {
+  //   if(root->edges[i] != NULL) cout << (root->edges[i]->priority) << ' '; else cout << ' ';
+  // }
+  // cout << '\n';
+  //
+  // for(i = 0; i < MAX; i++) {
+  //   if(root->edges[i] != NULL) cout << (root->edges[i]->length) << ' '; else cout << ' ';
+  // }
+  // cout << '\n';
+  //
+  // for(i = 0; i < MAX; i++) {
+  //   if(root->edges[i] != NULL) cout << (root->edges[i]->word) << ' '; else cout << ' ';
+  // }
+  // cout << '\n';
+  //
+  // for(i = 0; i < MAX; i++) {
+  //   cout << (root->edges[0]->edges[i]) << ' ';
+  // }
+  // cout << '\n';
+  //
+  // for(i = 0; i < MAX; i++) {
+  //   if(root->edges[0]->edges[i] != NULL) cout << (root->edges[0]->edges[i]->priority) << ' '; else cout << ' ';
+  // }
+  // cout << '\n';
+  //
+  // for(i = 0; i < MAX; i++) {
+  //   if(root->edges[0]->edges[i] != NULL) cout << (root->edges[0]->edges[i]->length) << ' '; else cout << ' ';
+  // }
+  // cout << '\n';
+
+
 
   for(i = 0; i < N; i++) {
     cin >> temp;
-    cout << searchWord(root, temp, 0, 0) << '\n';
+    timer = 0;
+    searchWord(root, temp, 0, 0, true);
+    cout << timer << '\n';
+    // cout << searchWord() << '\n';
   }
 }
